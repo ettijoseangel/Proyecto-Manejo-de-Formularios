e270e53efa4571b4931baad29effd42da59024cf
Angel <etti.joseangel@gmail.com>
2026-05-12 23:21:40 -0600

    Segunda validación agregada: Coherencia entre Horario y Hora

--- formulario_desestructurado/script.js
+++ formulario_desestructurado/script.js
@@ -14,12 +14,45 @@ document
     const fecha = document.getElementById("fecha").value;
     const hora = document.getElementById("hora").value;
 
-    // Validaciones básicas
+    // 1. Validaciones básicas
     if (!nombre || !correo || !telefono || intereses.length === 0 || !horario) {
       alert("Por favor, completa todos los campos obligatorios.");
       return;
     }
 
+    // 2. Validacion de coherencia Horario vs Hora.
+    if (horario.value === "mañana") {
+      if (hora >= "12:00") {
+        alert(
+          'Has seleccionado "Mañana", por favor elige una hora antes de las 12:00 PM.',
+        );
+        return;
+      }
+    } else if (horario.value === "tarde") {
+      if (hora < "12:00" || hora >= "19:00") {
+        alert(
+          'Has seleccionado "Tarde", por favor elige una hora entre las 12:00 PM y las 07:00 PM.',
+        );
+        return;
+      }
+    } else if (horario.value === "noche") {
+      if (hora < "19:00") {
+        alert(
+          'Has seleccionado "Noche", por favor elige una hora a partir de las 07:00 PM.',
+        );
+        return;
+      }
+    }
+
+    // 3. Si pasa todas las validaciones
+    console.log("Datos del registro:", {
+      nombre,
+      correo,
+      horario: horarioValue,
+      hora,
+    });
+
     // Si todo está bien
     alert("Registro exitoso. ¡Gracias por registrarte!");
+    this.reset();
   });
